9eadae7b7159428995a504efb64c424b9b87a3ab
Smith7w7 <72725197+Smith7w7@users.noreply.github.com>
2021-11-22 00:36:55 -0500

    cambios arreglados

--- Choza-Nautica/app/Models/Clientes.php
+++ Choza-Nautica/app/Models/Clientes.php
@@ -5,30 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class combos extends Model
+class Clientes extends Model
 {
-    protected $fillable = [
-        'id_administrador',
-        'nombre',
-        'descripcion',
-        'precio',
-        'foto',
-        'estado',
-        'created_at',
-    ];
 
-    public function compras_detalles(){
-        return $this->hasMany(compras_detalles::class,'id_combo');
-    }
-    public function productos(){
-        return $this->belongsToMany(Productos::class);
-    }
-    public static function store_combos($datosCombos){
-        $combos_prod = self::create($datosCombos);
-        $combos_prod->productos()->attach($datosCombos['productos']);
-    }
-    public function combos_update($datosCombos){
-        $this->update($datosCombos);
-        $this->productos()->sync($datosCombos['productos']);
-    }
+   
+ 
 }
